36ada9e6c78bb7d88bc1860178a8761439c150be
Oleg <serv_all@mail.ru>
2022-07-29 21:38:09 +0300

    EF Core подключено и работает, БД SQLIte

---
+++ WebChat/Repository/EFMessagesRepository.cs
@@ -0,0 +1,44 @@
+﻿namespace WebChat;
+
+public class EFMessagesRepository : IRepository<UserMessage>
+{
+    private readonly DataContext dataContext;
+
+    public EFMessagesRepository(DataContext DataContext)
+    {
+        dataContext = DataContext;
+    }
+
+    public int Create(UserMessage item)
+    {
+        var db = dataContext;
+        db.UserMessages.Add(item);
+        db.SaveChanges();
+        int lastIndex = db.UserMessages
+            .OrderBy(m => m.Id)
+            .Last().Id;
+
+        return lastIndex;
+    }
+
+    public IEnumerable<UserMessage> GetAll()
+    {
+        var db = dataContext.UserMessages.ToArray();
+        return db;
+    }
+
+    public void Update(UserMessage item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public UserMessage Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WebChat/Repository/EFUserRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+namespace WebChat;
+
+public class EFUserRepository : IRepository<User>
+{
+    private readonly DataContext dataContext;
+
+    public EFUserRepository(DataContext DataContext)
+    {
+        dataContext = DataContext;
+    }
+
+    public int Create(User item)
+    {
+        var db = dataContext;
+        db.Users.Add(item);
+        db.SaveChanges();
+        int lastIndex = db.Users
+            .OrderBy(u => u.Id)
+            .Last().Id;
+
+        return lastIndex;
+    }
+
+    public User Get(int id)
+    {
+        var db = dataContext;
+
+        User user = db.Users.FirstOrDefault(u => u.Id == id);
+        return user;
+    }
+
+    public IEnumerable<User> GetAll()
+    {
+        var db = dataContext;
+        var returnValue = db.Users;
+        return returnValue;
+    }
+
+    public void Update(User item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
